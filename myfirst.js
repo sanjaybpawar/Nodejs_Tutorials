a8f579b44757b61ca7a7afcb517d495d6d066517
sanjay <44497586+sanjaybpawar@users.noreply.github.com>
2018-12-04 17:51:51 +0530

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.end('Hello World This is my first node js program!');
-}).listen(8080);
+}).listen(1234);
 server.listen(port, hostname, () => {
   console.log(`Server running....`);
 });
